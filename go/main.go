fd81c4b250222c45e3c3d697fe65ca426e56f11c
yu-ko-ba <yu-ko-ba@outlook.com>
2022-01-21 12:23:11 +0900

    入力欄になにかしら入力されたらGoで定義した関数を呼び出すようにした

--- go/main.go
+++ go/main.go
@@ -4,17 +4,30 @@ import (
 	"fmt"
 	"strconv"
 	"strings"
+	"syscall/js"
 	"time"
 )
 
 
 func main() {
-    dateOfBirth := "2023/01/20"
-    fmt.Println(howOldAmI(dateOfBirth))
+    c := make(chan struct{}, 0)
+    registerCallbacks()
+    <-c
 }
 
 
-func howOldAmI(dateOfBirth string) int {
+func registerCallbacks() {
+    js.Global().Set("howOldAmI", js.FuncOf(howOldAmI))
+}
+
+
+func howOldAmI(this js.Value, args []js.Value) interface{} {
+    fmt.Println(args)
+    return "hoge"
+}
+
+
+func calcAge(dateOfBirth string) int {
     now := time.Now()
     splitNow := strings.Split(now.Format("2006/01/02"), "/")
 
