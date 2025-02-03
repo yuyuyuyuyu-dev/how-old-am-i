23dd5a031bda93bcb8ca819732bc1f9c20f09d57
yu-ko-ba <59269129+yu-ko-ba@users.noreply.github.com>
2025-02-03 21:48:43 +0900

    feat(pwa): change cache strategy to "StaleWhileRevalidate"

--- composeApp/workbox-config.js
+++ composeApp/workbox-config.js
@@ -1,8 +1,10 @@
 module.exports = {
     globDirectory: "build/dist/wasmJs/productionExecutable/",
-    globPatterns: [
-        "**/*",
-    ],
-    swDest: "build/dist/wasmJs/productionExecutable/serviceWorker.js",
+    globPatterns: [],
     maximumFileSizeToCacheInBytes: 10 * 1024 * 1024,
+    runtimeCaching: [{
+        urlPattern: /.+/,
+        handler: "StaleWhileRevalidate",
+    }],
+    swDest: "build/dist/wasmJs/productionExecutable/serviceWorker.js",
 };
