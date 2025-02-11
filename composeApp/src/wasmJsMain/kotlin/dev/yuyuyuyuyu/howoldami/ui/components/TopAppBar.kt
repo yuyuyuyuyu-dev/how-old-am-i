48ffb20309df3ff134d0ff1b75726913ea0340dd
yu-ko-ba <59269129+yu-ko-ba@users.noreply.github.com>
2025-02-11 09:43:56 +0900

    refactor(ui.components): improve TopAppBar props

--- composeApp/src/wasmJsMain/kotlin/dev/yuyuyuyuyu/howoldami/ui/components/TopAppBar.kt
+++ composeApp/src/wasmJsMain/kotlin/dev/yuyuyuyuyu/howoldami/ui/components/TopAppBar.kt
@@ -1,11 +1,7 @@
 package dev.yuyuyuyuyu.howoldami.ui.components
 
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
-import androidx.compose.material.icons.automirrored.filled.OpenInNew
 import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
@@ -14,68 +10,17 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.UriHandler
-import androidx.compose.ui.unit.dp
-
-
-@Composable
-fun TopAppBar(
-    title: String,
-    navigateBackIsPossible: Boolean,
-    sourceCodeURL: String,
-    uriHandler: UriHandler,
-    onNavigateBackButtonClick: () -> Unit,
-    onNavigateThirdPartyLicensesButtonClick: () -> Unit,
-    modifier: Modifier = Modifier,
-) {
-    TopAppBar(
-        title = title,
-        navigateSourceCodeButtonLabel = {
-            Row {
-                Icon(Icons.AutoMirrored.Filled.OpenInNew, "open in new icon")
-                Spacer(Modifier.width(1.dp))
-                Text("ソースコード")
-            }
-        },
-        navigateBackIsPossible = navigateBackIsPossible,
-        onNavigateBackButtonClick = onNavigateBackButtonClick,
-        onNavigateThirdPartyLicensesButtonClick = onNavigateThirdPartyLicensesButtonClick,
-        onNavigateSourceCodeButtonClick = {
-            uriHandler.openUri(sourceCodeURL)
-        },
-    )
-}
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TopAppBar(
     title: String,
     navigateBackIsPossible: Boolean,
+    openSourceLicensesButtonLabel: @Composable () -> Unit = { Text("オープンソース ライセンス") },
+    sourceCodeButtonLabel: @Composable () -> Unit = { Text("ソースコード") },
     onNavigateBackButtonClick: () -> Unit,
-    onNavigateThirdPartyLicensesButtonClick: () -> Unit,
-    onNavigateSourceCodeButtonClick: (() -> Unit)? = null,
-    modifier: Modifier = Modifier,
-) {
-    TopAppBar(
-        title = title,
-        navigateSourceCodeButtonLabel = { Text("ソースコード") },
-        navigateBackIsPossible = navigateBackIsPossible,
-        onNavigateBackButtonClick = onNavigateBackButtonClick,
-        onNavigateThirdPartyLicensesButtonClick = onNavigateThirdPartyLicensesButtonClick,
-        onNavigateSourceCodeButtonClick = onNavigateSourceCodeButtonClick,
-        modifier = modifier,
-    )
-}
-
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-private fun TopAppBar(
-    title: String,
-    navigateSourceCodeButtonLabel: @Composable () -> Unit,
-    navigateBackIsPossible: Boolean,
-    onNavigateBackButtonClick: () -> Unit,
-    onNavigateThirdPartyLicensesButtonClick: () -> Unit,
-    onNavigateSourceCodeButtonClick: (() -> Unit)? = null,
+    onOpenSourceLicensesButtonClick: () -> Unit,
+    onSourceCodeButtonClick: (() -> Unit)? = null,
     modifier: Modifier = Modifier,
 ) {
     TopAppBar(
@@ -104,17 +49,17 @@ private fun TopAppBar(
                 onDismissRequest = { menuIsExpanded = false },
             ) {
                 DropdownMenuItem(
-                    text = { Text("サードパーティーライセンス") },
+                    text = openSourceLicensesButtonLabel,
                     onClick = {
-                        onNavigateThirdPartyLicensesButtonClick()
+                        onOpenSourceLicensesButtonClick()
                         menuIsExpanded = false
                     },
                 )
-                if (onNavigateSourceCodeButtonClick != null) {
+                if (onSourceCodeButtonClick != null) {
                     DropdownMenuItem(
-                        text = navigateSourceCodeButtonLabel,
+                        text = sourceCodeButtonLabel,
                         onClick = {
-                            onNavigateSourceCodeButtonClick()
+                            onSourceCodeButtonClick()
                             menuIsExpanded = false
                         },
                     )
